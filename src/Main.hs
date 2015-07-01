b27df0f6e0973b90c401e8bfd52192814c0e6aa3
Chris Warburton <chriswarbo@gmail.com>
2015-07-01 15:34:28 +0100

    Label outputs

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,9 @@
 module Main where
 
-main = return ()
+import MLSpec.Theory
+import System.Environment
+
+main = do stdin <- getContents
+          [dir] <- getArgs
+          dirs  <- writeTheoriesFromClusters dir stdin
+          mapM (putStrLn . ("PROJECT " ++)) dirs
