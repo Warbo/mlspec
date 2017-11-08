21dc4f03712c6a9d02292c3ebca559e38aeadcfb
Chris Warburton <chriswarbo@gmail.com>
2017-11-08 19:54:46 +0000

    Speed up tests by removing nix-shell usage and 1-bucket list-extras

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@
 # integration tests.
 
 function fail {
-    echo "FAIL: $1" >> /dev/stderr
+    echo -e "FAIL: $*" 1>&2
     exit 1
 }
 
@@ -16,8 +16,6 @@ echo "Running MLSpec test suite"
 cabal test --show-details=streaming || fail "cabal test failed"
 echo "Successfully ran test suite"
 
-rm -f ./*.tix
-
 echo "Running integration test executable"
 cabal run mlspec-test-quickspec || fail "mlspec-test-quickspec failed"
 echo "Successfully ran integration test"
@@ -28,10 +26,8 @@ echo "Running MLSpec on example data"
 
 for FILE in test-data/*format*
 do
-    rm -f ./*.tix
-
     echo "Checking '$FILE' is valid JSON"
-    LENGTH=$(nix-shell -p jq --run "jq 'length'" < "$FILE") ||
+    LENGTH=$(jq 'length' < "$FILE") ||
         fail "Couldn't read '$FILE' as JSON"
 
     EXPECT=$(basename "$FILE" | rev | cut -d '.' -f1 | rev)
@@ -42,7 +38,7 @@ do
     OUTPUT=$(cabal run -v0 MLSpec < "$FILE") ||
         fail "MLSpec failed for stdin from '$FILE'"
     JSON=$(echo "$OUTPUT" | grep -v "^Depth")
-    EQS=$(echo "$JSON" | nix-shell -p jq --run "jq -s 'length'") ||
+    EQS=$(echo "$JSON" | jq -s 'length') ||
         fail "Found no JSON for stdin '$FILE'. Got '$JSON'"
     echo "Found '$EQS' equations from '$FILE'"
 
@@ -50,7 +46,7 @@ do
     OUTPUT=$(cabal run -v0 MLSpec "$FILE") ||
         fail "MLSpec failed for argument '$FILE'"
     JSON=$(echo "$OUTPUT" | grep -v "^Depth")
-    EQS=$(echo "$JSON" | nix-shell -p jq --run "jq -s 'length'") ||
+    EQS=$(echo "$JSON" | jq -s 'length') ||
         fail "Found no JSON for argument '$FILE'. Got '$JSON'"
     echo "Found '$EQS' equations from '$FILE'"
 done
